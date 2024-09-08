681c3c97870fbb6c0cd8042ebf1423cac38fbbdd
jbrough <jamie1612@gmail.com>
2024-09-08 16:40:20 +0100

    export TrackType

--- src/fmp4/mod.rs
+++ src/fmp4/mod.rs
@@ -2,9 +2,9 @@
 pub use self::common::Mp4Box;
 pub use self::initialization::{
     AacSampleEntry, AvcConfigurationBox, AvcSampleEntry, ChunkOffsetBox, DataEntryUrlBox,
-    DataInformationBox, DataReferenceBox, EditBox, EditListBox, FileTypeBox, HandlerReferenceBox,
-    InitializationSegment, MediaBox, MediaHeaderBox, MediaInformationBox, MovieBox,
-    MovieExtendsBox, MovieExtendsHeaderBox, MovieHeaderBox, Mpeg4EsDescriptorBox,
+    DataInformationBox, DataReferenceBox, EditBox, EditListBox, FileTypeBox, FlacSampleEntry,
+    HandlerReferenceBox, InitializationSegment, MediaBox, MediaHeaderBox, MediaInformationBox,
+    MovieBox, MovieExtendsBox, MovieExtendsHeaderBox, MovieHeaderBox, Mpeg4EsDescriptorBox,
     SampleDescriptionBox, SampleEntry, SampleSizeBox, SampleTableBox, SampleToChunkBox,
     SoundMediaHeaderBox, TimeToSampleBox, TrackBox, TrackExtendsBox, TrackHeaderBox,
     VideoMediaHeaderBox,
@@ -12,6 +12,7 @@ pub use self::initialization::{
 pub use self::media::{
     MediaDataBox, MediaSegment, MovieFragmentBox, MovieFragmentHeaderBox, Sample, SampleFlags,
     TrackFragmentBaseMediaDecodeTimeBox, TrackFragmentBox, TrackFragmentHeaderBox, TrackRunBox,
+    TrackType,
 };
 
 const VIDEO_TRACK_ID: u32 = 1;
